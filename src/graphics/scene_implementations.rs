14f02d0b756b9bbbff276764de51aa757a93d9d1
Tom <smithtom6304@gmail.com>
2023-11-29 21:26:37 +0000

    Remove unneeded fields and methods for tracking nodes in graphics state

--- src/graphics/scene_implementations.rs
+++ src/graphics/scene_implementations.rs
@@ -12,8 +12,6 @@ pub trait Scene {
     fn resize(&mut self, new_size: (u32, u32));
     fn input(&mut self, event: &sdl2::event::Event) -> bool;
     fn update(&mut self);
-    fn add_node_to_scene(&mut self, node: node::Node);
-    fn remove_node_from_scene(&mut self, id: node::NodeId);
     fn render(
         &mut self,
         clear_colour: wgpu::Color,
