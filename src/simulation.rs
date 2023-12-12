96169485393b3afa52df1a59163ae12160b22651
Tom Smith <tsmith@opera.com>
2023-12-12 15:10:51 +0000

    Add simulation tests

--- src/simulation.rs
+++ src/simulation.rs
@@ -2,7 +2,6 @@ use super::node;
 
 pub struct Simulation {
     pub nodes: Vec<node::Node>,
-    // Graphics
 }
 
 impl<'a> Simulation {
@@ -18,8 +17,45 @@ impl<'a> Simulation {
     pub fn remove_node(&mut self, id: node::NodeId) {
         self.nodes.retain(|node| node.id != id)
     }
+}
 
-    pub fn get_node(&'a self, id: node::NodeId) -> Option<&'a node::Node> {
-        self.nodes.iter().find(|node| node.id == id)
+#[cfg(test)]
+mod a_simulation {
+    use crate::node::{Node, NodeId, NodePosition};
+
+    use super::*;
+
+    #[test]
+    fn has_no_nodes_on_creation() {
+        let simulation = Simulation::new();
+        assert!(simulation.nodes.is_empty());
+    }
+
+    #[test]
+    fn can_add_node() {
+        let mut simulation = Simulation::new();
+        let id = NodeId(1);
+        let position = NodePosition { x: 3, y: 5 };
+        let node = Node::new(id, position);
+
+        simulation.add_node(node.clone());
+
+        assert_eq!(1, simulation.nodes.len());
+        assert_eq!(&node, simulation.nodes.get(0).unwrap());
+    }
+
+    #[test]
+    fn can_remove_node() {
+        let mut simulation = Simulation::new();
+        let id = NodeId(1);
+        let position = NodePosition { x: 3, y: 5 };
+        let node = Node::new(id, position);
+
+        simulation.add_node(node.clone());
+
+        simulation.remove_node(id);
+
+        assert_eq!(0, simulation.nodes.len());
+        assert!(!simulation.nodes.contains(&node));
     }
 }
